644695f0b3e0974a2caa0d9c8484759bdafeafdc
diegton <diegton_rodrigues@hotmail.com>
2019-12-08 16:30:12 -0300

    input on change test

--- src/components/mainComponent.vue
+++ src/components/mainComponent.vue
@@ -14,6 +14,8 @@
 
 <script>
 const axios = require("axios").default;
+import { GOOGLE_API_KEY, OPENWEATHERMAP_KEY } from "@/API_KEY";
+
 export default {
   name: "mainComponent",
   props: {},
@@ -29,32 +31,28 @@ export default {
   },
   methods: {
     async getData() {
-      const GOOGLE_API_KEY = this.$store.getters.getGOOGLE_API_KEY;
-      const OPENWEATHERMAP_KEY = this.$store.getters.getOPENWEATHERMAP_KEY;
-
-      console.log(OPENWEATHERMAP_KEY);
-
       const address = this.endereco;
       try {
         const response = await axios.get(
           `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`
         );
+        console.log("a");
         if (response.data.results.length > 0) {
           this.lat = response.data.results[0].geometry.location.lat;
           this.lng = response.data.results[0].geometry.location.lng;
           this.formatted_address = response.data.results[0].formatted_address;
           this.weather = "";
 
-          try {
-            const weather = await axios.get(
-              `https://openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lng}&appid=${OPENWEATHERMAP_KEY}`
-            );
-            console.log(weather);
-            this.weatherIcon = `http://openweathermap.org/img/wn/${weather.data.weather[0].icon}.png`;
-            this.weatherDescription = weather.data.weather[0].description;
-          } catch (error) {
-            console.log(error);
-          }
+          // try {
+          //   const weather = await axios.get(
+          //     `https://openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lng}&appid=${OPENWEATHERMAP_KEY}`
+          //   );
+          //   console.log(weather);
+          //   this.weatherIcon = `http://openweathermap.org/img/wn/${weather.data.weather[0].icon}.png`;
+          //   this.weatherDescription = weather.data.weather[0].description;
+          // } catch (error) {
+          //   console.log(error);
+          // }
         } else {
           this.formatted_address = "endereço não encontrado";
           this.lat = "";
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,9 +1,40 @@
 import { shallowMount } from '@vue/test-utils'
 import mainComponent from '@/components/mainComponent.vue'
+import axios from 'axios'
+import { GOOGLE_API_KEY, OPENWEATHERMAP_KEY } from "@/API_KEY";
+
+jest.mock('axios')
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const wrapper = shallowMount(mainComponent, {})
-    expect(wrapper.text()).toMatch('');
+
+  const endereco = 'cardoso';
+
+  axios.get.mockImplementation(() => Promise.resolve({
+    data: {
+      results: [{
+        geometry: {
+          location: {
+            lat: 1,
+            lng: 2
+          }
+        },
+        formatted_address: endereco
+      }]
+    }
+  }))
+
+  const wrapper = shallowMount(mainComponent, {})
+  it('input on change', async () => {
+    expect(wrapper.vm.endereco).toBe('')
+    const input = wrapper.find('input')
+    input.setValue(endereco)
+    input.trigger('change')
+    await wrapper.vm.$nextTick();
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(`https://maps.googleapis.com/maps/api/geocode/json?address=${endereco}&key=${GOOGLE_API_KEY}`)
+    expect(wrapper.vm.lat).toBe(1)
+    expect(wrapper.vm.lng).toBe(2)
+    expect(wrapper.vm.formatted_address).toBe(endereco)
+
   })
 })
